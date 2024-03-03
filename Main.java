79e4f4e8b56e8475363a27f127251c6b89f400ee
WeConnected <40178706+KvasirQc@users.noreply.github.com>
2024-03-03 14:00:19 +0000

    abc

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int international;
+        char character;
+        float floating;
+        boolean trueOrFalse;
+        string textString;
+
+        int international = 120;
+        char character = "d";
+        float floating = 18.99f;
+        boolean trueOrFalse = "true";
+        String textString = "This is my string of text";
+        final char noteExam = "A"
+
+        System.out.print("Hello the World");
+        System.out.print(" -WeConnected ");
+        System.out.print(33 + 33);
+        System.out.print("e du nom!");
+    }
+
+}
